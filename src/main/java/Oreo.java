011c77d185c475821126c5f106dbc967e9f60627
Ronith <ronithsaju2002@gmail.com>
2025-08-22 13:09:06 +0800

    Echo inputs

--- src/main/java/Oreo.java
+++ src/main/java/Oreo.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
+
 public class Oreo {
     public static void horizontalLine() {
         System.out.println("____________________________________________________________");
     }
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         horizontalLine();
         System.out.println("Hello! I'm Oreo");
         System.out.println("What can I do for you?");
         horizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        horizontalLine();
+
+        while (true) {
+            String userInput = input.nextLine();
+
+            if (userInput.equals("bye")) {
+                horizontalLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                horizontalLine();
+                break;
+            }
+
+            horizontalLine();
+            System.out.println(userInput);
+            horizontalLine();
+        }
     }
 }
