b4840f244d2dce95e56a77d5477708d5862acdeb
Ronith <ronithsaju2002@gmail.com>
2025-08-22 12:47:41 +0800

    Rename class, greet and exit

---
+++ src/main/java/Oreo.java
@@ -0,0 +1,13 @@
+public class Oreo {
+    public static void horizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        horizontalLine();
+        System.out.println("Hello! I'm Oreo");
+        System.out.println("What can I do for you?");
+        horizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        horizontalLine();
+    }
+}
