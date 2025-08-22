1de8d4fa267e42f14d1f52e49ba0137823074e8e
Ronith <ronithsaju2002@gmail.com>
2025-08-22 13:29:22 +0800

    Store inputs as tasks

--- src/main/java/Oreo.java
+++ src/main/java/Oreo.java
@@ -6,6 +6,8 @@ public class Oreo {
     }
 
     public static void main(String[] args) {
+        int count = 1;
+        String[] tasks = new String[100];
         Scanner input = new Scanner(System.in);
 
         horizontalLine();
@@ -21,11 +23,22 @@ public class Oreo {
                 System.out.println("Bye. Hope to see you again soon!");
                 horizontalLine();
                 break;
+            } else if (userInput.equals("list")) {
+                horizontalLine();
+                for (int i = 1; i < count; i++) {
+                    System.out.println(i + ". " + tasks[i]);
+                }
+                horizontalLine();
+                continue;
             }
 
+            tasks[count] = userInput;
+
             horizontalLine();
-            System.out.println(userInput);
+            System.out.println("added: " + userInput);
             horizontalLine();
+
+            count++;
         }
     }
 }
